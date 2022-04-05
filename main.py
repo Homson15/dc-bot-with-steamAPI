f47ead97c6c760b47e53fc829534cd7222160147
Bartosz Choma <bartosz.choma@skni.umcs.pl>
2022-04-06 00:26:09 +0200

    Work in progress

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
+from steamAPI.Steam import getSteam
 
 
 if __name__ == '__main__':
-    pass
+
+    steamAPI = getSteam()
+
 
--- steamAPI/App.py
+++ steamAPI/App.py
@@ -1,6 +1,19 @@
 
+import requests
 
 class App:
 
-    def __init__(self, id, name):
+    def __init__(self, id, name, isDLC):
 
+        self.id = id
+        self.name = name
+
+        self.url = f'https://store.steampowered.com/api/appdetails?appids={id}'
+
+        self.isDLC = isDLC;
+
+
+
+    def print(self):
+
+        print(self.id, self.name, self.isDLC)
