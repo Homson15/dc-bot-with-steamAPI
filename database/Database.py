e8aee021571b395183c9db9205048328007d0bd5
Bartosz Choma <bartosz.choma@skni.umcs.pl>
2022-04-15 11:33:29 +0200

    adding database

---
+++ database/Database.py
@@ -0,0 +1,176 @@
+import os
+from sqlalchemy import Column, Integer, String, create_engine, MetaData, Boolean, inspect, update, delete
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from steamAPI.App import App
+
+database = None
+
+
+def getDatabase():
+
+    global database
+
+    if database is None:
+        database = Database()
+
+    return database
+
+
+
+class Database:
+
+    base = create_engine("sqlite:///database/gamesInfo.db")
+
+    MD = MetaData()
+
+    BaseModel = declarative_base(metadata=MD)
+
+    class GamesInfo(BaseModel):
+        __tablename__ = "GamesInfo"
+        appID = Column(Integer, primary_key=True)
+        name = Column(String(100), nullable=False)
+        isGame = Column(Boolean, nullable=False)
+        type = Column(String(100))
+        parentID = Column(Integer)
+        developers = Column(String(100))
+        currency = Column(String(10), nullable=False)
+        initialPrice = Column(Integer, nullable=False)
+        discount = Column(Integer, nullable=False)
+        finalFormatted = Column(String(20))
+
+    class SubscridebGames(BaseModel):
+        __tablename__ = "SubscribedGames"
+        appID = Column(Integer, primary_key=True)
+        name = Column(String(100), nullable=False)
+        discount = Column(Integer, nullable=False)
+        finalFormatted = Column(String(20))
+
+
+
+    def __init__(self):
+        self.BaseModel.metadata.create_all(self.base)
+        self.mapper = inspect(self.GamesInfo)
+
+
+
+    def putData(self, app : App):
+
+        DBSesion = sessionmaker(bind=self.base)
+        session = DBSesion()
+
+        values = app.getValues()
+
+        session.add(self.GamesInfo(
+            appID=values["appID"],
+            name=values["name"],
+            isGame=values["isGame"],
+            type=values["type"],
+            parentID=values["parentID"],
+            developers=values["developers"],
+            currency=values["currency"],
+            initialPrice=values["initialPrice"],
+            discount=values["discount"],
+            finalFormatted=values["priceFormatted"]
+        ))
+
+        session.commit()
+        session.close()
+
+
+    def getRecordByName(self, name):
+
+        DBSesion = sessionmaker(bind=self.base)
+        session = DBSesion()
+
+        arr = []
+
+        for each in session.query(self.GamesInfo):
+            if name in each.name:
+                arr.append(App(
+                    each.appID,
+                    each.name,
+                    each.isGame,
+                    each.type,
+                    each.parentID,
+                    each.developers,
+                    each.currency,
+                    each.initialPrice,
+                    each.discount
+                ))
+
+        return arr
+
+    def subscribe(self, app : App):
+
+        DBSesion = sessionmaker(bind=self.base)
+        session = DBSesion()
+
+        values = app.getValues()
+
+        session.add(self.SubscridebGames(
+            appID=values["appID"],
+            name=values["name"],
+            discount=values["discount"],
+            finalFormatted=values["priceFormatted"]
+        ))
+
+        session.commit()
+        session.close()
+
+
+    def getSubscribed(self):
+
+        DBSesion = sessionmaker(bind=self.base)
+        session = DBSesion()
+
+        arr = []
+
+        for each in session.query(self.SubscridebGames):
+            arr.append(App(
+                each.appID,
+                each.name,
+                discount=each.discount,
+                finalFormatted=each.finalFormatted
+            ))
+
+        return arr
+
+    def modifySubbscribed(self, app: App):
+
+        DBSesion = sessionmaker(bind=self.base)
+        session = DBSesion()
+
+        session.query(self.SubscridebGames).update().where(self.SubscridebGames.appID==app.appID).values(
+            discount = app.discount,
+            priceFormatted = app.priceFormatted
+        )
+
+        session.commit()
+        session.close()
+
+
+    def deleteSubscribed(self, app: App):
+
+        DBSesion = sessionmaker(bind=self.base)
+        session = DBSesion()
+
+        session.query(self.SubscridebGames).delete().where(self.SubscridebGames.appID==app.appID)
+
+        session.commit()
+        session.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- steamAPI/App.py
+++ steamAPI/App.py
@@ -3,17 +3,69 @@ import requests
 
 class App:
 
-    def __init__(self, id, name, isDLC):
+    def __init__(self, id, name, isGame=False, type=None, parent=None, developers=None, currency="PLN", price=0, finalPrice=0, discount=0, finalFormatted=""):
 
-        self.id = id
+        self.appID = id
         self.name = name
+
+        self.isGame = isGame
+        self.type = type
+        self.parent = parent
+        self.developers = developers
+        self.currency = currency
+        self.initialPrice = price
+        self.finalPrice = finalPrice
+        self.discount = discount
+        self.priceFormatted = finalFormatted
 
         self.url = f'https://store.steampowered.com/api/appdetails?appids={id}'
 
-        self.isDLC = isDLC;
+        self.valuesSet = False
+
+    def selfSetValues(self):
+
+        request = requests.get(self.url).json()
+
+        if not request:
+            print(f"Request returns NULL for {self.appID} ({self.name})")
+            return False
+
+        try:
+            data = request[f"{self.appID}"]["data"]
+
+            self.type = data["type"]
+
+            if self.type=="game":
+                self.isGame = True
+            else:
+                self.parent = data["fullgame"]["appid"]
+
+            self.developers = data["developers"][0]
+
+            if not data["is_free"]:
+
+                price = data["price_overview"]
+
+                self.currency = price["currency"]
+                self.initialPrice = price["initial"]
+                self.finalPrice = price["final"]
+                self.discount = price["discount_percent"]
+
+                self.priceFormatted = price["final_formatted"]
+
+            self.valuesSet = True
+
+            return True
+
+        except KeyError:
+            print(f"Error while geting values for {self.appID} ({self.name}) \n"
+                  f"Gathering data stopped...")
+            return False
+
+    def getValues(self):
+
+        return {"appID": self.appID, "name": self.name, "isGame": self.isGame, "type": self.type, "parentID": self.parent,
+                "developers": self.developers, "currency": self.currency, "initialPrice": self.initialPrice, "finalPrice": self.finalPrice,
+                "discount": self.discount, "priceFormatted": self.priceFormatted}
 
 
-
-    def print(self):
-
-        print(self.id, self.name, self.isDLC)
